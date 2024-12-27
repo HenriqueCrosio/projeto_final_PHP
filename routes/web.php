6ff3bd9474e183a418b007a90644b67498c81e7b
HenriqueCrosio <henriquecrosio@hotmail.com>
2024-12-27 19:14:21 +0000

    Implementação da segunda parte do projeto
    - Criados os controllers para Autores e Livros.
    - Implementadas as rotas necessárias.
    - Adicionadas validações nos controllers.
    - Corrigido erro de sincronizaçao da primeira parte do projeto

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,10 +13,18 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Rotas de Livros
+    Route::resource('books', BookController::class);
+    Route::get('/books/search', [BookController::class, 'search'])->name('books.search');
+
+    // Rotas de Autores
+    Route::resource('authors', AuthorController::class);
 });
 
 require __DIR__.'/auth.php';
+
